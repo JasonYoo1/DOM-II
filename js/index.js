cec086ba20069f5dfb7033126601aadd507b508a
JasonYoo1 <jyoojs@gmail.com>
2019-06-18 12:50:06 -0700

    added mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+
+let navLink = document.getElementById('something');
+
+navLink.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+
+
+// document.addEventListener('keydown', logKey);
+
+// function logKey(e) {
+//   log.textContent += ` ${e.code}`;
+// }
