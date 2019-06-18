060fc30d32d555205f5b22302b8274119828e6a4
JasonYoo1 <jyoojs@gmail.com>
2019-06-18 12:58:08 -0700

    wheel zoom on navbar

--- js/index.js
+++ js/index.js
@@ -19,3 +19,25 @@ document.addEventListener('keydown', logKey);
 function logKey(e) {
   log.textContent += ` ${e.code}`;
 }
+
+
+//WHEEL ON NAVBAR
+
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('div');
+  el.onwheel = zoom;
+
+  el.addEventListener('wheel', zoom);
+
+
+//
